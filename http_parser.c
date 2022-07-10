23213e67b774a3ae09291c96ef6e992c958d869c
Vladislav <sdvlad75@gmail.com>
2022-07-10 15:06:55 +0300

    First commit! Added simple multi-threaded server written in C.

---
+++ http_parser.c
@@ -0,0 +1,3 @@
+//
+// Created by vladislav on 10.07.22.
+//
---
+++ http_parser.h
@@ -0,0 +1,8 @@
+//
+// Created by vladislav on 10.07.22.
+//
+
+#ifndef SERVER_HTTP_PARSER_H
+#define SERVER_HTTP_PARSER_H
+
+#endif //SERVER_HTTP_PARSER_H
